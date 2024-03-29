ee2f37f338fcaa82448dbc034032ffb6ca238d3a
sunwooiljoon <wnsdl1659@naver.com>
2024-03-29 13:18:50 +0900

    feat : access log 설정
    docker-entrypoint.sh
    - gunicorn 사용 배포 시 access log 기록하도록 옵션 추가

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,4 @@ set -e
 python manage.py makemigrations
 python manage.py migrate
 
-gunicorn -b 0.0.0.0 -p 8000 userManagement.wsgi:application
+gunicorn -b 0.0.0.0 -p 8000 --access-logfile ./access.log userManagement.wsgi:application
