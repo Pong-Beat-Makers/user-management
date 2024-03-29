1cc92517f981fb56c3f8f9d27f67469ea68cc497
sunwooiljoon <wnsdl1659@naver.com>
2024-03-29 16:48:21 +0900

    fix : docker-entrypoint.sh 수정
    - makemigrations 에서 app name 추가

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 set -e
-python manage.py makemigrations
+python manage.py makemigrations social_login friends
 python manage.py migrate
 
 gunicorn -b 0.0.0.0 -p 8000 --access-logfile ./access.log userManagement.wsgi:application
