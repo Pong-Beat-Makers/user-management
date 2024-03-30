0e430bb4158e673a1aed00e55fe062d427054381
youngmin <youngmin@youngminui-MacBookPro.local>
2024-03-30 15:32:59 +0900

    frinedlist->friend

--- friends/urls.py
+++ friends/urls.py
@@ -4,6 +4,6 @@ from . import views
 # localhost:8000/friends/
 urlpatterns = [
     path('', views.FriendshipView.as_view(), name='friend'),
-    path('friendlist/', views.s2s_FriendshipView.as_view(), name='s2s_friendship'),
+    path('friend/', views.s2s_FriendshipView.as_view(), name='s2s_friendship'),
     path('friendaddme/', views.s2s_FriendAddMeview.as_view(), name='s2s_user_as_friend'),
 ]
